92150be86b04dd61981678e966b92c066a38ab01
Ehsan Pourhadi <ehsan.pourhadi@telavox.com>
2026-01-11 15:50:27 +0100

    added deploy to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets are loaded correctly on GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
